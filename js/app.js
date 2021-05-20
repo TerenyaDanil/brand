676500c835cbedc9a3a39dbf0c5939bb7c46d610
TerenyaDanil <47912455+TerenyaDanil@users.noreply.github.com>
2021-05-20 20:18:43 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 var swiper = new Swiper('.rev__slider', {
-	loop: true,
 	spaceBetween: 100,
 	grabCursor: true,
-	effect: "fade",
+	autoHeight: true,
 	simulateTouch: true,
 	centeredSlides: true,
 	slidesPerView: 1,
@@ -53,6 +52,66 @@ for (const pluses of plus) {
 
 	}
 }
+
+
+more = document.querySelector('.program__btn');
+courseInner = document.querySelector('.inner-program-hide');
+
+
+
+more.onclick = function () {
+	courseInner.classList.toggle('active');
+	more.hidden = true;
+
+}
+
+
+
+////////////////
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 10;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
+
+
+
+
+
+
+////////////////////////
 // Dynamic Adapt v.1
 // HTML data-da="where(uniq class name),position(digi),when(breakpoint)"
 // e.x. data-da="item,2,992"
